11e81a20aab8708801a6c8358a1ef9f2d6e5e5b5
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2024-02-05 13:48:18 +0200

    Add files via upload

--- 04.php
+++ 04.php
@@ -9,6 +9,7 @@
   <body>
     <div class="container">
     <h1>Harjutus 04</h1>
+    <h4>jagamine</h4>
 
 
 
@@ -18,9 +19,9 @@
         arv2  <input type="text" name="j2"><br>
         <input type="submit" value="Arvuta!">
     </form>
-  
     <?php
-    
+    #tõnis kändmaa
+    #05.02.24 it-222
     if(!empty($_GET["j1"]) && !empty($_GET["j2"]) ){
     
 
@@ -38,36 +39,81 @@
        // echo 'vastus on on' .$u. '<br>';
 
       
-    
-      
       ?>
+      <p>_________________________________________________________________________________________________________________________________________________________</p>
 
-
+<h4>vanuse asi</h4>
+<form action="#" method="get">
+        <p>sisestage 2 isiku vanus</p>
+vanus1 <input type="text" name="vanus1"><br>
+        vanus2  <input type="text" name="vanus2"><br>
+        <input type="submit" value="Leia vahe!">
+    </form>
 <?php
-    //harjutus 4
-    //tõnis kdandmaa
-    //01.02.24
-    
+    $vanus1 = $_GET['vanus1'];
+    $vanus2 = $_GET['vanus2'];
    
-    
-    
-
-
-    
-
-
-    
-   // mingi if 0 siis kirjutab  if(!empty($_GET[‘name’])){lause}
-
-
-
-
-
-
-
-
+ if($vanus1 > $vanus2){
+  echo '1. isik on vanem';
+  } else if($vanus1 < $vanus2) {
+  echo '2. isik on vanem';
+  } else if($vanus1 = $vanus2) 
+  echo 'Te olete sama vanusega';
+   else if ($vanus1 && $vanus2 = 0) {
+  echo 'Sisestage vanus';  
+  }  
 
 ?>
+
+<p>_________________________________________________________________________________________________________________________________________________________</p>
+<h4>ristkulik eta ruut</h4>
+<form action="#" method="get">
+        <p>sisetage küljed</p>
+        kulg 1 = <input type="text" name="kulg1"><br>
+        kulg 2 = <input type="text" name="kulg2"><br>
+        <input type="submit" value="Leia kujund!">
+    </form>
+
+<?php
+$ristkulikpilt='<img src="harjutus4ristkulik.png"/>';
+$ruutpilt='<img src="harjutus4ruut.png"/>';
+
+    $kulg1 = $_GET['kulg1'];
+    $kulg2 = $_GET['kulg2'];
+   
+    if($kulg1 == $kulg2){
+      echo 'vastus on ruut';
+      echo $ruutpilt;
+      } else {
+      echo 'vastus on ristkülik';
+      echo $ristkulikpilt;
+      }
+      
+?>
+
+<p>_________________________________________________________________________________________________________________________________________________________</p>
+<h4>juubel</h4>
+<form action="#" method="get">
+        <p>sisesta enda vanus</p>
+        vanus = <input type="text" name="juubel"><br>
+        <input type="submit" value="Leia kujund!">
+    </form>
+
+    <?php
+
+    $juubel = $_GET['juubel'];
+   
+    if($juubel == 0 && 5){
+      echo 'teil on juube';
+      } else {
+      echo 'ei ole';
+   
+      }
+      
+?>
+
+
+
  </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
